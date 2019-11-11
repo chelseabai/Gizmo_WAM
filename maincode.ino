0654f8da97660530adb309318a1a6afbecf96cb8
chelseabai <53417086+chelseabai@users.noreply.github.com>
2019-11-11 02:16:42 +0000

    Add files via upload

--- maincode.ino
+++ maincode.ino
@@ -1,13 +1,62 @@
 #include <Servo.h>
+#include <Arduino.h>
+#include <TM1637Display.h>
+
+
+// define the time duration here
+int count = 120; 
+uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
+
+// before the game '----'
+const uint8_t NONE[] = {
+  SEG_G,           
+  SEG_G,   
+  SEG_G,               
+  SEG_G
+  };
+
+// start the game 'go'
+const uint8_t START[] = {
+  0,           
+  0,   
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_F | SEG_G,                     
+  SEG_C | SEG_D | SEG_E | SEG_G             
+  };
+
+// end the game 'done'
+const uint8_t END[] = {
+  0,           
+  SEG_A | SEG_E | SEG_D | SEG_F | SEG_G,   
+  SEG_C | SEG_E | SEG_G,                    
+  SEG_B | SEG_C | SEG_D | SEG_E | SEG_G         
+  };
+
+// best score 'best'
+const uint8_t BEST[] = {
+  SEG_C | SEG_D | SEG_E | SEG_F | SEG_G, 
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G,
+  SEG_A | SEG_C | SEG_D | SEG_F | SEG_G,
+  SEG_D | SEG_E | SEG_F | SEG_G               
+  };
+
+// define connection pins for timer here
+#define CLK A0
+#define DIO A1
+
+TM1637Display display(CLK, DIO);
 
 Servo Motor1;
 Servo Motor2;
 Servo Motor3;
+Servo Motor4;
+Servo Motor5;
 
 //change the motor pin here
 int MotorPin1 = 8;
 int MotorPin2 = 9;
 int MotorPin3 = 10;
+int MotorPin4 = 11;
+int MotorPin5 = 12;
 
 //change the angle here
 int angleUP = 90;
@@ -20,18 +69,23 @@ int easyUp = 3000;
 int easyDown = 5000;
 
 //time
+unsigned long previousMillisTime = 0;
 unsigned long previousMillis = 0;
 unsigned long previousMillis1 = 0;
 unsigned long previousMillis2 = 0;
 unsigned long previousMillis3 = 0;
+unsigned long previousMillis4 = 0;
+unsigned long previousMillis5 = 0;
 
 //change the sensor pin here
-int mainswitchPin = 5;
+int mainswitchPin = 7;
 int switchPin1 = 2;
 int switchPin2 = 3;
 int switchPin3 = 4;
+int switchPin4 = 5;
+int switchPin5 = 6;
 
-int soundPin = 7;
+int soundPin = 13;
 
 
 //initialise sensor readings
@@ -39,6 +93,9 @@ int mainpress = 0;
 int pressSwitch1 = 0;
 int pressSwitch2 = 0;
 int pressSwitch3 = 0;
+int pressSwitch4 = 0;
+int pressSwitch5 = 0;
+
 
 //set the sensor threshold here
 int threshold = 100;
@@ -56,6 +113,7 @@ int motorControl1 (int interval) {
     pressSwitch1 = digitalRead(switchPin1);
     Serial.println("Motor 1: UP");
     Serial.println(dif2);
+    countDown(); 
 
     
     if(pressSwitch1){
@@ -85,6 +143,7 @@ int motorControl2 (int interval) {
     pressSwitch2 = digitalRead(switchPin2);
     Serial.println("Motor 2: UP");
     Serial.println(dif2);
+    countDown(); 
 
         
     if(pressSwitch2){
@@ -113,6 +172,7 @@ int motorControl3 (int interval) {
     int dif2 = millis() - previousMillis3;
     Serial.println("Motor 3: UP");
     Serial.println(dif2);
+    countDown(); 
 
     if(pressSwitch3){
       digitalWrite(soundPin,HIGH);
@@ -134,8 +194,69 @@ int motorControl3 (int interval) {
   }
 }
 
+int motorControl4 (int interval) {
+  Motor4.write(angleUP);
+  while (true) {
+    int dif2 = millis() - previousMillis4;
+    Serial.println("Motor 4: UP");
+    Serial.println(dif2);
+    countDown(); 
+    
+    if(pressSwitch4){
+      digitalWrite(soundPin,HIGH);
+      Serial.println("Motor 4: HIT");
+      Motor4.write(angleInitial);
+      Serial.println("Motor 4: DOWN");
+      wins++;
+      return wins;
+      break;
+    }
+    
+    if (dif2 > interval){
+      Serial.println("Motor 4: BREAK");
+      Motor4.write(angleInitial);
+      Serial.println("Motor 4: DOWN");
+      return wins;
+      break;
+    }
+  }
+}
 
+int motorControl5 (int interval) {
+  Motor5.write(angleUP);
+  while (true) {
+    int dif2 = millis() - previousMillis5;
+    Serial.println("Motor 5: UP");
+    Serial.println(dif2);
+    countDown(); 
 
+    if(pressSwitch5){
+      digitalWrite(soundPin,HIGH);
+      Serial.println("Motor 5: HIT");
+      Motor5.write(angleInitial);
+      Serial.println("Motor 5: DOWN");
+      wins++;
+      return wins;
+      break;
+    }
+    
+    if (dif2 > interval){
+      Serial.println("Motor 5: BREAK");
+      Motor5.write(angleInitial);
+      Serial.println("Motor 5: DOWN");
+      return wins;
+      break;
+    }
+  }
+}
+
+int countDown(){
+  data[3] = display.encodeDigit((9-(millis()-6000)/1000 % 10));
+  data[2] = display.encodeDigit((11-(millis()-6000)/10000 % 10)%10);
+  data[1] = display.encodeDigit((120000-millis()+6000)/100000 % 10);
+  data[0] = display.encodeDigit(0);
+  display.setSegments(data);
+}
 
 //main code
 
@@ -165,14 +286,21 @@ void setup() {
 }
 
 void loop() {
+  display.clear();
+  display.setBrightness(0x0f);
+  display.setSegments(NONE);
   mainpress = digitalRead(mainswitchPin);
-  if(mainpress){
+  if (mainpress){
+    display.setSegments(START);
     Serial.println("Start!");
     delay(6000);
     previousMillis = millis();
     previousMillis1 = millis();
     previousMillis2 = millis();
     previousMillis3 = millis();
+    previousMillis4 = millis();
+    previousMillis5 = millis();
+    previousMillisTime = millis();
     while(true){
       int dif = millis() - previousMillis;
       Serial.print(dif);
@@ -181,23 +309,36 @@ void loop() {
       previousMillis1 = millis();
       previousMillis2 = millis();
       previousMillis3 = millis();
-      Serial.println(".");
-    
+      previousMillis4 = millis();
+      previousMillis5 = millis();
+      Serial.println("."); 
+      countDown();       
+     
       //total game time
-      if (millis()  > 60000) {
+      if (millis()  > 126000) {
         Serial.println("END");
         Serial.print("Score: ");
         Serial.println(wins);
+        display.setSegments(END);
+        delay(3000);
         if (wins > highScore){
           highScore = wins;
           Serial.println("Highest Score!");
+          for (int n=0; n<3; n++){
+            display.showNumberDec(wins);
+            delay(1000);
+            display.setSegments(BEST);
+            delay(1000);
+          }
+        } else{
+          display.showNumberDec(wins);
+          delay(6000);
         }
-        delay(2000);
         exit(0);
       }
       
       if (dif >= easyDown) {
-        randMotor = random(1);
+        randMotor = random(5);
         if (randMotor == 0) {
           Serial.print("Choose Motor 1");
           motorControl1(easyUp);
@@ -225,8 +366,26 @@ void loop() {
           previousMillis = millis();
           Serial.println(previousMillis);
         }  
-//        easyDown = easyDown - 300;
-//        easyUp = easyUp - 300;
+        if (randMotor == 3) {
+          Serial.println("Choose Motor 4");
+          motorControl4(easyUp);
+          delay(200);
+          digitalWrite(soundPin,LOW);  
+          Serial.println("low");
+          previousMillis = millis();
+          Serial.println(previousMillis);
+        }  
+        if (randMotor == 4) {
+          Serial.println("Choose Motor 5");
+          motorControl5(easyUp);
+          delay(200);
+          digitalWrite(soundPin,LOW);  
+          Serial.println("low");
+          previousMillis = millis();
+          Serial.println(previousMillis);
+        }
+      easyDown = easyDown - 300;
+      easyUp = easyUp - 300;
       }
     }    
   }
